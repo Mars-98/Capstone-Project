4cc84d177474d1dac88582e522ab658b00d463e9
willwohlt <wlwohlt@uwm.edu>
2021-11-28 09:08:11 -0600

    working site navigation. no implementation of gameplay

--- screens/Game.js
+++ screens/Game.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Welcome from './../screens/Welcome';
+import SwappableGrid from "../components/SwappableGrid";
+import { Dimensions } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+let playButton = require("../assets/PlayButton.png");
+let justClouds = require("../assets/CloudsBackground.png");
+
 import {
     InnerContainer,
     PageTitle,
@@ -13,9 +20,15 @@ import {
 } from './../components/styles';
 
 
+
 const Game = ({navigation, route}) => {
     return (
         <>
+            <View style={styles.backGroundImage}>
+                <ImageBackground source={justClouds} style={styles.backGroundImage}>
+                    <SwappableGrid/>
+                </ImageBackground>
+            </View>
             <StatusBar style="dark"/>
             <InnerContainer>
                 <WelcomeContainer>
@@ -31,5 +44,33 @@ const Game = ({navigation, route}) => {
         </>
     );
 }
+
+let Window = Dimensions.get("window");
+let windowSpan = Math.min(Window.width, Window.height);
+let colored = true;
+let TILE_WIDTH = windowSpan / 6;
+
+let windowWidth = Window.width;
+let windowHeight = Window.height;
+
+let blue = colored ? "#3c44d8" : "#ffffff";
+let red = colored ? "#f24646" : "#ffffff";
+let yellow = colored ? "#faff7f" : "#ffffff";
+let green = colored ? "#168e3a" : "#ffffff";
+let orange = colored ? "#ea0e62" : "#ffffff";
+let pink = colored ? "#ff51f3" : "#ffffff";
+let white = "#ffffff";
+
+let styles = StyleSheet.create({
+  backGroundImage: {
+    flex: 1,
+    width: windowWidth,
+    height: windowHeight,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+});
 
 export default Game;
